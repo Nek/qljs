7ddc1ed40718e1365ba9ccc36cccdd1e724c3680
Nikita Dudnik <nikdudnik@gmail.com>
2018-11-28 14:24:55 +0100

    Move multimethod into the main lib file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,43 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { first, zip } from './utils'
 
+const noMatch = term => {
+  throw new Error('No match for ' + term)
+}
+
+export function multimethod(dispatch) {
+  const dict = {}
+  if (typeof noMatch == 'function') {
+    dict.noMatch = noMatch
+  }
+
+  return new Proxy(
+    () => {
+      throw new Error('No match')
+    },
+    {
+      set(target, property, value) {
+        dict[property] = value
+        return true
+      },
+      get(target, property, receiver) {
+        return dict[property]
+      },
+      apply(target, thisArg, args) {
+        const value = dispatch.apply(null, args)
+        const func =
+          value && dict.hasOwnProperty(value)
+            ? dict[value]
+            : dict.hasOwnProperty('noMatch')
+              ? dict['noMatch']
+              : target
+        return func.apply(thisArg, args)
+      },
+    },
+  )
+}
+
+
 let Component
 let element
 let state
